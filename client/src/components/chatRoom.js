b9adda66cbc1acaaa6fd33ffc1143968451ac150
Pavel <pavelyeganov@gmail.com>
2018-09-10 21:37:32 -0400

    newherokuname

--- client/src/components/chatRoom.js
+++ client/src/components/chatRoom.js
@@ -18,7 +18,7 @@ class ChatRoom extends Component {
       user: {}
     };
 
-    this.socket = io("https://peaceful-badlands-20801.herokuapp.com/", {
+    this.socket = io("https://learndeckgl.herokuapp.com/", {
       extraHeaders: {
         Authorization: localStorage.getItem("jwtToken")
       }
